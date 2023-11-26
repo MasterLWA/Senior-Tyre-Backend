72dad579caf915190595812a6c5f00d829e35ce0
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2023-11-26 23:46:32 +0530

    Done and Dusted

--- Controller/ProfitController.js
+++ Controller/ProfitController.js
@@ -1,53 +1,49 @@
 const Profit = require('../Model/Profit'); // Use a capitalized name for the model
 const mongoose = require('mongoose');
 
-
-// Add profit
-const addProfit = async (req, res) => {
-    const { item, unitCostPrice, unitSellingPrice, totalSellingItems, totalSellingPrice, profit } = req.body;
-
-    try {
-        const newProfit = new Profit({
-            item,
-            unitCostPrice,
-            unitSellingPrice,
-            totalSellingItems,
-            totalSellingPrice,
-            profit
-        });
-
-        const savedProfit = await newProfit.save();
-
-        res.status(201).json(savedProfit); // Use 201 status code for resource creation
-    } catch (error) {
+    const addProfit = async (req, res) => {
+        try {
+        const profitData = req.body;
+    
+        // If it's an array, validate and create each object
+        if (Array.isArray(profitData)) {
+            const validationPromises = profitData.map(data => new Profit(data).validate());
+            await Promise.all(validationPromises);
+            const savedProfitArray = await Profit.create(profitData);
+            res.status(201).json(savedProfitArray);
+        } else { // If it's a single object, validate and create it
+            await new Profit(profitData).validate();
+            const savedProfit = await Profit.create(profitData);
+            res.status(201).json(savedProfit);
+        }
+        } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Server error' });
+        res.status(400).json({ error: error.message });
+        }
+    };
+
+    // Get all profits
+    const getAllProfit = async (req, res) => {
+        try {
+            const profits = await Profit.find({}); // Use the plural form 'profits' for the variable
+            res.status(200).json(profits);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Server error' });
+        }
     }
-}
 
 
-// Get all profits
-const getAllProfit = async (req, res) => {
-    try {
-        const profits = await Profit.find({}); // Use the plural form 'profits' for the variable
-        res.status(200).json(profits);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+    //get time by time duration
+    const getProfitByTimeDuration = async (req, res) => {
+        try {
+            const profits = await Profit.find({ day: { $gte: req.params.from, $lte: req.params.to } });
+            res.status(200).json(profits);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Server error' });
+        }
     }
-}
-
-
-//get time by time duration
-const getProfitByTimeDuration = async (req, res) => {
-    try {
-        const profits = await Profit.find({ day: { $gte: req.params.from, $lte: req.params.to } });
-        res.status(200).json(profits);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
-    }
-}
 
 module.exports = {
     addProfit,
